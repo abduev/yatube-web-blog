54b80d7d4b00daf0f6f488ae9717be162ab550d9
Zurab <zurab.abduev@gmail.com>
2021-01-22 21:54:54 +0300

    PEP8 is checked

--- posts/urls.py
+++ posts/urls.py
@@ -20,8 +20,9 @@ urlpatterns = [
         views.add_comment,
         name="add_comment"
     ),
-    path("<str:username>/follow/",
-        views.profile_follow, 
+    path(
+        "<str:username>/follow/",
+        views.profile_follow,
         name="profile_follow"
     ),
     path(
